6983a9976a6c34da6002bdee8d37b80e1ac2b923
Brenno C. Lins <dev@bcl-st.com.br>
2023-10-22 07:27:32 -0300

    feat: removendo a pasta DIST do projeto

--- dist/routes/get-apresentation.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getApresentation = void 0;
-async function getApresentation(app) {
-    app.get('/', () => {
-        return 'OpenApp API - API para abrir app no Windows ou Linux!';
-    });
-}
-exports.getApresentation = getApresentation;
